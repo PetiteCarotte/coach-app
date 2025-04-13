7f470bebfbf6f1df2a6d591993dc9bb569a50355
Caroline Petit <caroline.petit-4@etu.univ-tours.fr>
2025-04-13 05:24:18 +0200

    refactor: back: enregistrement des observateurs au démarrage de l'application

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,12 @@ from models import Program
 from utils.insert_data import insert_default_programs  
 from utils.insert_data import insert_default_slots
 from factories.user_factory import UserFactory
+from utils.event_manager import EventManager
+from observers.email_observer import EmailObserver
+from observers.logging_observer import LoggingObserver
+
+# Initialisation de l'EventManager (Singleton)
+event_manager = EventManager()
 
 routes(app)
 if __name__ == '__main__':
@@ -13,5 +19,11 @@ if __name__ == '__main__':
         db.create_all()  # Crée les tables si elles n'existent pas
         insert_default_programs()
         insert_default_slots()
+
+    # Enregistrer les observateurs
+    event_manager.subscribe("reservation_created", EmailObserver())
+    event_manager.subscribe("reservation_created", LoggingObserver())
+    event_manager.subscribe("reservation_cancelled", EmailObserver())
+    event_manager.subscribe("reservation_cancelled", LoggingObserver())
 
     app.run(debug=True)
