505b8f4e07bb2230d30daa1a1693a256330037fa
Caroline Petit <caroline.petit-4@etu.univ-tours.fr>
2025-04-13 22:56:57 +0200

    refactor: back: introduction d'un Decorateur d'authentification pour la protection des routes

---
+++ backend/utils/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from flask import request, jsonify
+import jwt
+
+SECRET_KEY = 'supersecretkey'
+
+def requires_auth(f):
+    """Décorateur pour vérifier l'authentification via JWT."""
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization', '').replace('Bearer ', '')
+        if not token:
+            return jsonify({'error': 'Token manquant'}), 401
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'error': 'Token expiré'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'error': 'Token invalide'}), 401
+        return f(*args, **kwargs)
+    return decorated
